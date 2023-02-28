05fe19e8f0749b2a8b59f8ed97889562dff36811
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-02-28 11:53:42 -0500

    starting rebuild

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 
-function App() {
+function App(): JSX.Element {
+  const [number, setNumber] = useState<number>(0);
+  const [input, setInput] = useState<string>('');
+
+  const jacobsthal = useCallback((n: number): number => {
+    if (n < 2) return n;
+    return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+  }, []);
+
+  const calculation = jacobsthal(number);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>test</h1>
-      </header>
+    <div>
+      <main>
+        <section>
+          <div className='user-input'>
+            <input
+              type='text'
+              value={number}
+              placeholder='desired index'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setNumber(e.target.valueAsNumber);
+              }}
+            />
+          </div>
+          <div className='result'>{calculation || 0}</div>
+        </section>
+        <section>
+          <div className='user-input'>
+            <input
+              type='text'
+              value={input}
+              placeholder='user input'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setInput(e.target.value);
+              }}
+            />
+          </div>
+          <div className='result'>{input || '--'}</div>
+        </section>
+      </main>
     </div>
   );
 }
