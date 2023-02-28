2a5b60ff48bbf0f12409c58ede89c794a27216fd
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-02-28 13:47:14 -0500

    refactor to ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 
-function App(): JSX.Element {
+function App() {
   const [number, setNumber] = useState<number>(0);
   const [input, setInput] = useState<string>('');
 
   const jacobsthal = useCallback((n: number): number => {
+    if (!n) return 0;
     if (n < 2) return n;
     return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
   }, []);
 
-  const calculation = jacobsthal(number);
+  const calculation = (n: number) => {
+    if (!n) {
+      return jacobsthal(0);
+    }
+    return jacobsthal(n);
+  };
 
   return (
     <div>
@@ -17,15 +23,16 @@ function App(): JSX.Element {
         <section>
           <div className='user-input'>
             <input
-              type='text'
-              value={number}
+              autoFocus
+              type='number'
+              value={number || ''}
               placeholder='desired index'
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 setNumber(e.target.valueAsNumber);
               }}
             />
           </div>
-          <div className='result'>{calculation || 0}</div>
+          <div className='result'>{calculation(number) || 0}</div>
         </section>
         <section>
           <div className='user-input'>
